8eeb5f8abee8e9cf428b1f528c9be31c620bbb9d
Eduardo Valle <77079061+Eduardovll@users.noreply.github.com>
2025-03-03 23:53:45 -0300

    Environment configuration

--- src/main/java/com/valledev/banksystem/BankSystemApplication.java
+++ src/main/java/com/valledev/banksystem/BankSystemApplication.java
@@ -2,12 +2,14 @@ package com.valledev.banksystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class BankSystemApplication {
+public class BankSystemApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankSystemApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(BankSystemApplication.class, args);}
+
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World");
 	}
-
 }
